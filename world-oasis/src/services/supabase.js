998ce81e0e39ec95b12d1d3392a7d8cca098320a
BENJAMIN DARTEY <95862472+Benjilo12@users.noreply.github.com>
2024-05-22 15:35:57 +0000

    connect with supabase

---
+++ world-oasis/src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://afieotbnxywmqtcuelka.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmaWVvdGJueHl3bXF0Y3VlbGthIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYwMzc0MTMsImV4cCI6MjAzMTYxMzQxM30.pKtaocVIc95BXf4_JA41XVrsePrI4ZwPQm4JtUXwjoo";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
