632bc137f3a5292c7bcc527b9444b79ffcac0462
BENJAMIN DARTEY <95862472+Benjilo12@users.noreply.github.com>
2025-01-31 06:42:02 +0000

    setting login form to default settings

--- world-oasis/src/features/authentication/LoginForm.jsx
+++ world-oasis/src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("benjamindarteyy@gmail.com");
-  const [password, setPassword] = useState("Optimus419");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
